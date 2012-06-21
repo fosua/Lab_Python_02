cc26ab9d2a1bf5e6a67263507b20c4e76795efbf
amo-nyarko esther <pcspida@ubuntu.(none)>
2012-06-21 01:28:24 +0000

    lab02s

--- problem6.py
+++ problem6.py
@@ -15,4 +15,27 @@ elif personAge < thePresidentAge:
 elif personAge >= theRetirementAge:
     print 'too old'
 else:
-    print 'well i have nothing to say about your age
+    print 'well i have nothing to say about your age'
+ #question 7  
+i = 40
+while i >=0:
+	x = i*3
+	if x<40:
+		print x,
+	i = i-1
+	#question 8
+for i in range (6, 30):
+    if i % 2 != 0:
+        if i&3 != 0:
+            if i % 5 !=0:
+                print i,
+	
+
+
+	# question 9
+	
+n=0
+while ((79*n) % 97) != 1:
+	n = n+1
+print n
+
